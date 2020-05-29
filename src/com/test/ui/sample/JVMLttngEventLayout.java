7b3d44d3ab15cdaaca61ccdd325ad78532cfc20a
Darya Razumova <d.razumova@yandex.ru>
2020-05-29 16:32:55 +0300

    fixed event layout

--- src/com/test/ui/sample/JVMLttngEventLayout.java
+++ src/com/test/ui/sample/JVMLttngEventLayout.java
@@ -4,52 +4,26 @@ import org.eclipse.tracecompass.internal.lttng2.ust.core.trace.layout.DefaultUst
 import org.eclipse.tracecompass.internal.lttng2.ust.core.trace.layout.LttngUst28EventLayout;
 
 @SuppressWarnings("restriction")
-public class JVMLttngEventLayout extends LttngUst28EventLayout {
+public class JVMLttngEventLayout extends LttngUst20EventLayout {
 	private static final JVMLttngEventLayout INSTANCE = new JVMLttngEventLayout();
 	
-	@Override
+	    @Override
     public String eventCygProfileFuncEntry() {
-        return "method__entry";
+        return "hotspot:method__entry";
     }
 
     @Override
     public String eventCygProfileFastFuncEntry() {
-        return eventCygProfileFuncEntry();
+        return "hotspot:method__entry";
     }
 
     @Override
     public String eventCygProfileFuncExit() {
-        return "method__return";
+        return "hotspot:method__return";
     }
 
     @Override
     public String eventCygProfileFastFuncExit() {
-        return eventCygProfileFuncExit();
+        return "hotspot:method__return";
     }
-    
-    public static JVMLttngEventLayout getInstance() {
-        return INSTANCE;
-    }
-    
-    @Override
-    public String contextVpid() {
-    	return "context._vpid";
-    }
-    
-    @Override
-    public String contextVtid() {
-    	return "context._vtid";
-    }
-    
-    @Override
-    public String contextProcname() {
-    	return "context._procname";
-    }
-    
-    @Override
-    public String contextIp() {
-    	return "context._ip";
-    }
-    
-    
 }
